b6c5c97d5575511d207410b196aa9eea226ba154
BrooitsFeiskJR <luizantonio0125@gmail.com>
2023-05-30 22:14:45 -0300

    Created Authentication Firebase Repository

---
+++ app/src/main/java/dev/tontech/job_finder_yt/data/repositories/AuthenticationFirebaseRepository.kt
@@ -0,0 +1,14 @@
+package dev.tontech.job_finder_yt.data.repositories
+
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+
+class AuthenticationFirebaseRepository(private val auth: FirebaseAuth) {
+    fun loginWithEmailAndPassword(email: String, password: String): Task<AuthResult> {
+        return auth.signInWithEmailAndPassword(email, password)
+    }
+    fun createUserWithEmailAndPassword(email: String, password: String): Task<AuthResult> {
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
+}
